8287838c0e78c72a7198a75f626b2f7788137bc8
The Demon <kartikbhalla12@gmail.com>
2017-08-21 00:04:38 +0530

    add download directory lister

---
+++ downloads/resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,17 @@
+<style>
+	a.foot:hover { 
+    text-decoration:none;
+	color:#fff;
+	}
+
+	#main {
+	padding-top:10px;
+	text-align: center !important;
+	font-weight: bold;
+	}
+</style>
+<div id="main"><a class="foot" href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
+<br>
+<div class="footer">
+    Copyright © 2017 HyperX. All Rights Reserved.
+</div>
